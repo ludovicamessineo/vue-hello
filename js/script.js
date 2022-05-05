d2579b1ed8c6116f3229ebca89a49b428e0cdbcc
ludovicamessineo <ludovica.messineo9@gmail.com>
2022-05-05 17:14:21 +0200

    add vue exercise

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const app = new Vue (
+    {
+        el: '#root',
+        data: {
+            message: 'Me trying to figure out Js 🙂:',
+            myImg: 'https://www.stardust.it/uploads/img/post/img/91ee1d5c86bd0831e38117fdb2f17f6d1594826596-optimized.jpg',
+        }
+    }
+)
